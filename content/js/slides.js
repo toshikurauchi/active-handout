e75d18cf0e864f08723074813164bd761a6c1694
Igor Montagner <igordsm@gmail.com>
2021-05-26 18:42:54 -0300

    Teste para usar preact ao inves de ficar criando elementos na mao. Ainda nao ficou legal, pois preciso criar o componente raiz na mao...

--- content/js/slides.js
+++ content/js/slides.js
@@ -1,32 +1,34 @@
-{
-    let current_document = window.location.href;
-    let last_slash = current_document.lastIndexOf("/");
+import { html, render } from 'https://unpkg.com/htm/preact/index.mjs?module'
 
-    let slide_path = "";
-    if (last_slash == current_document.length-1) {
-        slide_path = current_document + "slides.pdf";
-    } else {
-        slide_path = current_document.substring(0, last_slash) + "/slides.pdf";
-    }
-    
-    let xhr = new XMLHttpRequest();
-    xhr.addEventListener("load", function(ev) {
-        if (xhr.status == 200) {       
-            let emb = document.createElement("embed");
-            emb.src = slide_path;
-            emb.width = 500;
-            emb.height = 375;
-            emb.type = "application/pdf";
-            
-            let div_center = document.createElement("div");
-            div_center.style = "text-align: center";
-            div_center.className = "no-print";
-            div_center.appendChild(emb);
-            
-            document.querySelector("h1").insertAdjacentElement("afterend", div_center);
-        }
-    });
-    xhr.open("head", slide_path);
-    xhr.send();
-
+function PdfEmbed(props) {
+    return html`<embed src=${props.src} type="application/pdf"
+                 width=${props.width} height=${props.height}> </embed>`;
 }
+PdfEmbed.defaultProps = {
+    'width': '600',
+    'height': '375'
+};
+
+let current_document = window.location.href;
+let last_slash = current_document.lastIndexOf("/");
+
+let slide_path = "";
+if (last_slash == current_document.length-1) {
+    slide_path = current_document + "slides.pdf";
+} else {
+    slide_path = current_document.substring(0, last_slash) + "/slides.pdf";
+}
+
+fetch(slide_path, {'method': 'HEAD'}).then((response) => {
+    return response.status;
+}).then((st) => {
+    if (st == 200) {
+        let div_center = document.createElement("div");
+        div_center.style = "text-align: center";
+        div_center.className = "no-print";
+
+        render(html`<${PdfEmbed} src=${slide_path} />`, div_center);
+        
+        document.querySelector("h1").insertAdjacentElement("afterend", div_center);
+    }
+});
